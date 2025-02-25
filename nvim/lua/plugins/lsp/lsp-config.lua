73095f4f09df7ced6af7bf78b2b533b864907846
Liam Adamson <liamadamson1996@gmail.com>
2025-02-25 21:07:24 +0000

    Require rust-analyzer

--- nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/lua/plugins/lsp/lsp-config.lua
@@ -7,13 +7,8 @@ return {
     { "folke/neodev.nvim", opts = {} },
   },
   config = function()
-    -- import lspconfig plugin
     local lspconfig = require("lspconfig")
-
-    -- import mason_lspconfig plugin
     local mason_lspconfig = require("mason-lspconfig")
-
-    -- import cmp-nvim-lsp plugin
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
     local keymap = vim.keymap -- for conciseness
@@ -77,6 +72,10 @@ return {
       local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
+
+    mason_lspconfig.setup {
+      ensure_installed = {"rust_analyzer"}
+    }
 
     mason_lspconfig.setup_handlers({
       -- default handler for installed servers
